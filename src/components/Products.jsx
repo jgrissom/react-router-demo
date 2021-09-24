f46e1ecd243e6d6a6f8ec13becb818337678e5b3
jgrissom <jgrissom91@gmail.com>
2021-09-24 18:18:31 -0500

    [1.12] Link components

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Products(props) {
   const { products } = props;
@@ -11,7 +12,7 @@ function Products(props) {
       <ul>
         { products.map(p => 
           <li key={p.id}>
-            { p.name }
+            <Link to={`/product/${p.id}`}>{p.name}</Link>
           </li>
         )}
       </ul>
