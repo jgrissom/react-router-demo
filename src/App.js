89378d37123ce61e0f35f9d785088cb8f4197de9
jgrissom <jgrissom91@gmail.com>
2021-09-24 18:15:01 -0500

    [1.11] props

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ import NoMatch from "./components/NoMatch";
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 function App() {
+  const products = [
+    { id: 1, name: 'Goldfish', price: 1.99 },
+    { id: 2, name: 'Gummy Bears', price: 2.99 },
+    { id: 3, name: 'Yarn', price: 3.99 }
+  ]
   return (
     <Router>
       <div>
@@ -27,7 +32,7 @@ function App() {
           <Home />
         </Route>
         <Route exact path="/products">
-          <Products />
+          <Products products={ products } />
         </Route>
         <Route exact path="/about">
           <About />
